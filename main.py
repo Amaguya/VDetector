67b520f7508a6b0c786098c8527f3aabdfabe10d
Amaguya <k19shuhei@gmail.com>
2025-12-28 15:21:16 +0900

    play mp3 ok

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from tkinter import ttk
 import pyaudio
 import numpy as np
 from datetime import datetime
+import pygame
+import time
 
+DETECT_VOLUME = 1000
+SLEEP_TIME = 50 #ms
+#SOUND_PATH = "./sound/puff.mp3"
+SOUND_PATH = "./sound/guwa.mp3"
+
+# PyAudio Settings
 CHUNK = 2048
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -32,6 +40,48 @@ label.pack(padx=20, pady=20)
 log_box = tk.Listbox(root, height=5, width=50)
 log_box.pack()
 
+# Play Sound
+def play_wav(filename):
+    # Open WAV file
+    wav_file = wave.open(filename, 'rb')
+
+    # Initialize PyAudio for playback
+    audio_player = pyaudio.PyAudio()
+
+    # Open output stream
+    playback_stream = audio_player.open(
+        format=audio_player.get_format_from_width(wav_file.getsampwidth()),
+        channels=wav_file.getnchannels(),
+        rate=wav_file.getframerate(),
+        output=True
+    )
+    # Playback chunk size
+    playback_chunk = 1024
+
+    # Read first chunk
+    playback_data = wav_file.readframes(playback_chunk)
+
+    # Play until no more data
+    while playback_data:
+        playback_stream.write(playback_data)
+        playback_data = wav_file.readframes(playback_chunk)
+
+    # Cleanup
+    playback_stream.stop_stream()
+    playback_stream.close()
+    audio_player.terminate()
+    
+def play_mp3(filename):
+    # Initialize mixer
+    pygame.mixer.init()
+    # Load MP3 file
+    pygame.mixer.music.load(filename)
+    # Play
+    pygame.mixer.music.play()
+    # Cool Down
+    time.sleep(0.3)
+
+# Loop Function
 def update_meter():
     try:
         # Read audio chunk from the microphone
@@ -55,7 +105,7 @@ def update_meter():
         #meter["value"] = min(volume, 300)
         label.config(text=f"volume: {int(volume)}")
         
-        if int(volume) >= 5000:           
+        if int(volume) >= DETECT_VOLUME:           
             # Create timestamp string
             now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -74,6 +124,9 @@ def update_meter():
             # Append to log file
             with open("logs.txt", "a") as f:
                 f.write(log_message + "\n")
+                
+            #Play Sound
+            play_mp3(SOUND_PATH)
 
     except Exception as e:
         # Print any unexpected errors and continue running
@@ -81,7 +134,7 @@ def update_meter():
         volume = 0
 
     # Schedule the next update after 50 ms
-    root.after(100, update_meter)
+    root.after(SLEEP_TIME, update_meter)
 
 update_meter()
 
